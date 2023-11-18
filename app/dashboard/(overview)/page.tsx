7795c5a1bea342dca62acabfb0dee7c93000eb54
Henrique Degrecci <hdegrecci@gmail.com>
2023-11-18 13:48:04 -0300

    Chapter 9 - Suspense InvoicesChart

--- app/dashboard/(overview)/page.tsx
+++ app/dashboard/(overview)/page.tsx
@@ -1,14 +1,16 @@
+import {
+  LatestInvoicesSkeleton,
+  RevenueChartSkeleton,
+} from "@/app/ui/skeletons";
 import { fetchCardData, fetchLatestInvoices } from "@/app/lib/data";
 
 import { Card } from "@/app/ui/dashboard/cards";
 import LatestInvoices from "@/app/ui/dashboard/latest-invoices";
 import RevenueChart from "@/app/ui/dashboard/revenue-chart";
-import { RevenueChartSkeleton } from "@/app/ui/skeletons";
 import { Suspense } from "react";
 import { lusitana } from "@/app/ui/fonts";
 
 export default async function Page() {
-  const latestInvoices = await fetchLatestInvoices();
   const {
     totalPaidInvoices,
     totalPendingInvoices,
@@ -35,7 +37,9 @@ export default async function Page() {
         <Suspense fallback={<RevenueChartSkeleton />}>
           <RevenueChart />
         </Suspense>
-        <LatestInvoices latestInvoices={latestInvoices} />
+        <Suspense fallback={<LatestInvoicesSkeleton />}>
+          <LatestInvoices />
+        </Suspense>
       </div>
     </main>
   );
