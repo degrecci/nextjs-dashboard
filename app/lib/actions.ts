5b4c1ff25b9feb4542a3966a156470b1573eb492
Henrique Degrecci <hdegrecci@gmail.com>
2023-11-23 18:52:59 -0300

    Chapter 12 - Insert, Revalidate and Redirect

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -1,5 +1,8 @@
 "use server";
 
+import { redirect } from "next/navigation";
+import { revalidatePath } from "next/cache";
+import { sql } from "@vercel/postgres";
 import { z } from "zod";
 
 const FormSchema = z.object({
@@ -18,4 +21,15 @@ export async function createInvoice(formData: FormData) {
     amount: formData.get("amount"),
     status: formData.get("status"),
   });
+
+  const amountInCents = amount * 100;
+  const date = new Date().toISOString().split("T")[0];
+
+  await sql`
+    INSERT INTO invoices (customer_id, amount, status, date)
+    VALUES (${customerId}, ${amountInCents}, ${status}, ${date})
+  `;
+
+  revalidatePath("/dashboard/invoices");
+  redirect("/dashboard/invoices");
 }
