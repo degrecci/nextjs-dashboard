af272ba519e3e0ffcd5a5391f97760d03de37258
Henrique Degrecci <hdegrecci@gmail.com>
2023-11-25 11:33:15 -0300

    Chapter 14 - Validate create-form fields

--- app/ui/invoices/create-form.tsx
+++ app/ui/invoices/create-form.tsx
@@ -11,10 +11,13 @@ import { Button } from "@/app/ui/button";
 import { CustomerField } from "@/app/lib/definitions";
 import Link from "next/link";
 import { createInvoice } from "@/app/lib/actions";
+import { useFormState } from "react-dom";
 
 export default function Form({ customers }: { customers: CustomerField[] }) {
+  const initialState = { message: null, errors: {} };
+  const [state, dispatch] = useFormState(createInvoice, initialState);
   return (
-    <form action={createInvoice}>
+    <form action={dispatch}>
       <div className="rounded-md bg-gray-50 p-4 md:p-6">
         {/* Customer Name */}
         <div className="mb-4">
