06c1e0bf52054c46b52a0fc6171e4d6db0fd12e1
Henrique Degrecci <hdegrecci@gmail.com>
2023-11-30 18:41:23 -0300

    Chapter 15 - Authenticate function

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -2,6 +2,7 @@
 
 import { redirect } from "next/navigation";
 import { revalidatePath } from "next/cache";
+import { signIn } from "@/auth";
 import { sql } from "@vercel/postgres";
 import { z } from "zod";
 
@@ -109,5 +110,19 @@ export async function deleteInvoice(id: string) {
     return { message: "Deleted Invoice." };
   } catch (error) {
     return { message: "Database Error: Failed to Delete Invoice." };
+  }
+}
+
+export async function authenticate(
+  prevState: string | undefined,
+  formData: FormData,
+) {
+  try {
+    await signIn("credentials", Object.fromEntries(formData));
+  } catch (error) {
+    if ((error as Error).message.includes("CredentialsSignin")) {
+      return "CredentialsSignin";
+    }
+    throw error;
   }
 }
