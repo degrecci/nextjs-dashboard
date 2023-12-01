36165f4326beb293417a66ece6a73074843da6e0
Henrique Degrecci <hdegrecci@gmail.com>
2023-12-01 19:44:32 -0300

    Login error message

--- app/ui/login-form.tsx
+++ app/ui/login-form.tsx
@@ -13,7 +13,7 @@ import { authenticate } from "@/app/lib/actions";
 import { lusitana } from "@/app/ui/fonts";
 
 export default function LoginForm() {
-  const [state, dispatch] = useFormState(authenticate, undefined);
+  const [errorMessage, dispatch] = useFormState(authenticate, undefined);
   return (
     <form action={dispatch} className="space-y-3">
       <div className="flex-1 rounded-lg bg-gray-50 px-6 pb-4 pt-8">
@@ -67,10 +67,10 @@ export default function LoginForm() {
           aria-live="polite"
           aria-atomic="true"
         >
-          {state === "CredentialsSignin" && (
+          {errorMessage && (
             <>
               <ExclamationCircleIcon className="h-5 w-5 text-red-500" />
-              <p className="text-sm text-red-500">Invalid credentials</p>
+              <p className="text-sm text-red-500">{errorMessage}</p>
             </>
           )}
         </div>
